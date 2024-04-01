6460640f51573d2e176a07dc3ad14c1c907db321
Minh Nguyen <melnguyen115@gmail.com>
2024-04-01 02:05:12 -0700

    added camera zoom

--- Assets/Scripts/CameraSystem.cs
+++ Assets/Scripts/CameraSystem.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraSystem : MonoBehaviour {
     [SerializeField] private Camera cam;
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+    [SerializeField] private float minOrthographicSize;
+    [SerializeField] private float maxOrthographicSize;
 
     private Vector3 dragOrigin;
+    private float targetOrthographicSize = 6f;
 
     private void Update() {
         PanCamera();
+        ZoomCamera();
     }
 
     private void PanCamera() {
@@ -21,5 +27,19 @@ public class CameraSystem : MonoBehaviour {
 
             gameObject.transform.position += difference;
         }
+    }
+
+    private void ZoomCamera() {
+        if (Input.mouseScrollDelta.y > 0) {
+            targetOrthographicSize -= 2;
+        }
+        if (Input.mouseScrollDelta.y < 0) {
+            targetOrthographicSize += 2;
+        }
+
+        targetOrthographicSize = Mathf.Clamp(targetOrthographicSize, minOrthographicSize, maxOrthographicSize);
+
+        float zoomSpeed = 5f;
+        cinemachineVirtualCamera.m_Lens.OrthographicSize = Mathf.Lerp(cinemachineVirtualCamera.m_Lens.OrthographicSize, targetOrthographicSize, Time.deltaTime * zoomSpeed);
     }
 }
